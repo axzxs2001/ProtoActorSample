4ae78b402c2fa2ccdd75d8b384110cd64431e45f
桂素伟 <axzxs2001@163.com>
2018-08-13 09:50:32 +0900

    新建Proto.Actor Sample解决方案。 新建Proto.Actor.ppt 新建HelloWorld项目

---
+++ ProtoActorSample/HelloWorld/Program.cs
@@ -0,0 +1,37 @@
+﻿using Proto;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var props = Actor.FromProducer(() => new HelloActor());
+            var pid = Actor.Spawn(props);
+            pid.Tell(new Hello
+            {
+                Who = "Alex"
+            });
+            Console.ReadLine();
+        }
+    }
+
+    class Hello
+    {
+        public string Who;
+    }
+    class HelloActor : IActor
+    {
+        public Task ReceiveAsync(IContext context)
+        {
+            var msg = context.Message;
+            if (msg is Hello r)
+            {
+                Console.WriteLine($"Hello {r.Who}");
+            }
+            return Actor.Done;
+        }
+    }
+}
