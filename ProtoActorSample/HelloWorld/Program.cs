77e632d39936a27c1e3f4a9246546ecb9480604a
桂素伟 <axzxs2001@163.com>
2018-08-15 09:51:15 +0900

    完善Proto.Actor HelloWorld例子

--- ProtoActorSample/HelloWorld/Program.cs
+++ ProtoActorSample/HelloWorld/Program.cs
@@ -2,14 +2,18 @@
 using System;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace P001_HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {        
+
+            //Actor产生一个props（道具）
             var props = Actor.FromProducer(() => new HelloActor());
+            //从props衍生pid，pid代理一个actor的地址
             var pid = Actor.Spawn(props);
+            //把Hello对象交给HelloActor处理
             pid.Tell(new Hello
             {
                 Who = "Alex"
@@ -17,14 +21,15 @@ namespace HelloWorld
             Console.ReadLine();
         }
     }
-
+    //传递对象
     class Hello
     {
         public string Who;
     }
-
+    //actor
     class HelloActor : IActor
     {
+        //被调用
         public Task ReceiveAsync(IContext context)
         {
             var msg = context.Message;
