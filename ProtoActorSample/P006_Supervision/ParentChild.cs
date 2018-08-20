d2881d77b0b8842693f740a49a68a51df6ff4c6a
桂素伟 <axzxs2001@163.com>
2018-08-20 15:02:09 +0900

    Behaviors Demo Complete

---
+++ ProtoActorSample/P006_Supervision/ParentChild.cs
@@ -0,0 +1,42 @@
+﻿using Proto;
+using System;
+
+namespace P006_Supervision
+{
+    class ParentChild
+    {
+        static void Main(string[] args)
+        {
+            var childProps = Actor.FromFunc(context =>
+            {
+                Console.WriteLine($"子 Actor {context.Self.Id}: MSG: {context.Message.GetType()}");
+                switch (context.Message)
+                {
+                    case Started _:
+                        throw new Exception("child failure");
+                }
+                return Actor.Done;
+            });
+
+            var rootProps = Actor.FromFunc(context =>
+            {
+                Console.WriteLine($"父 Actor {context.Self.Id}: MSG: {context.Message.GetType()}");
+                switch (context.Message)
+                {
+                    case Started _:
+                        context.SpawnNamed(childProps, "child");
+                        break;
+                    case Terminated terminated:
+                        Console.WriteLine($"父 终止 {terminated.Who}");
+                        break;
+                }
+                return Actor.Done;
+            })
+            .WithChildSupervisorStrategy(new OneForOneStrategy((pid, reason) => SupervisorDirective.Escalate, 0, null));
+          
+            Actor.SpawnNamed(rootProps, "root");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProtoActorSample/P007_Behaviors/Program.cs
@@ -0,0 +1,89 @@
+﻿using Proto;
+using System;
+using System.Threading.Tasks;
+
+namespace P007_Behaviors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var props = Actor.FromProducer(() => new LightBulb());
+            var pid = Actor.Spawn(props);
+            var message = pid.RequestAsync<string>(new PressSwitch()).Result;
+            Console.WriteLine(message);
+            message = pid.RequestAsync<string>(new Touch()).Result;
+            Console.WriteLine(message);
+            message = pid.RequestAsync<string>(new PressSwitch()).Result;
+            Console.WriteLine(message);
+            message = pid.RequestAsync<string>(new Touch()).Result;
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+
+    public class LightBulb : IActor
+    {
+        private readonly Behavior _behavior;
+
+        public LightBulb()
+        {
+            _behavior = new Behavior();
+            _behavior.Become(Off);
+        }  
+        public Task ReceiveAsync(IContext context)
+        {
+            return _behavior.ReceiveAsync(context);
+        }
+        private Task Off(IContext context)
+        {
+            switch (context.Message)
+            {
+                case PressSwitch _:
+                    context.Respond("Turning on");
+                    _behavior.Become(On);
+                    break;
+                case Touch _:
+                    context.Respond("Cold");
+                    break;
+            }
+            return Actor.Done;
+        }
+        private Task On(IContext context)
+        {
+            switch (context.Message)
+            {
+                case PressSwitch _:
+                    context.Respond("Turning off");
+                    _behavior.Become(Off);
+                    break;
+                case Touch _:
+                    context.Respond("Hot!");
+                    break;
+            }
+            return Actor.Done;
+        }
+        private Task Smashed(IContext context)
+        {
+            switch (context.Message)
+            {
+                case PressSwitch _:
+                    context.Respond(""); // nothing happens!
+                    break;
+                case Touch _:
+                    context.Respond("Owwww!");
+                    break;
+                case ReplaceBulb _:
+                    _behavior.Become(Off);
+                    break;
+            }
+            return Actor.Done;
+        }
+    }
+    class PressSwitch
+    {}
+    class Touch
+    {}
+    class ReplaceBulb
+    {}
+}
