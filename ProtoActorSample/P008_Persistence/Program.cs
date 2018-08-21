f78d609c94c439a901d2e1744427122cbb9e0bc6
桂素伟 <axzxs2001@163.com>
2018-08-21 14:16:54 +0900

    Behavior PPT完成 创建Persistence项目

---
+++ ProtoActorSample/P008_Persistence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P008_Persistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
