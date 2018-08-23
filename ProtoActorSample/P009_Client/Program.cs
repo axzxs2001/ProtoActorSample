0e225ed512505048137662603ca03f2b90669e68
桂素伟 <axzxs2001@163.com>
2018-08-23 11:29:08 +0900

    Remote 项目创建

---
+++ ProtoActorSample/P009_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P009_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
