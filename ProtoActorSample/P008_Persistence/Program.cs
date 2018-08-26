882ef77b0e67d76b65e2cc35c93baf20ebd00560
桂素伟 <axzxs2001@163.com>
2018-08-26 13:36:01 +0900

    测试持久化不同方式 整理PPT

--- ProtoActorSample/P008_Persistence/Program.cs
+++ ProtoActorSample/P008_Persistence/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.Sqlite;
 using Proto;
 using Proto.Persistence;
+using Proto.Persistence.SnapshotStrategies;
 using Proto.Persistence.Sqlite;
 using System;
 using System.Threading.Tasks;
@@ -15,9 +16,31 @@ namespace P008_Persistence
             var actorid = "myactorid";
             var dbfile = @"C:\MyFile\Source\Repos\ProtoActorSample\ProtoActorSample\P008_Persistence\data.sqlite";
             var sqliteProvider = new SqliteProvider(new SqliteConnectionStringBuilder() { DataSource = dbfile });
-            var props = Actor.FromProducer(() => new DataActor(sqliteProvider, actorid));
-            var pid = Actor.Spawn(props);
 
+            Console.WriteLine("1、事件溯源   2、快照   3、带快照的事件溯源");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CallES(actorid, sqliteProvider);
+                    break;
+                case "2":
+                    CallSN(actorid, sqliteProvider);
+                    break;
+                case "3":
+                    CallES_SN(actorid, sqliteProvider);
+                    break;
+            }
+
+        }
+        /// <summary>
+        /// 事件溯源
+        /// </summary>
+        /// <param name="actorid"></param>
+        /// <param name="sqliteProvider"></param>
+        private static void CallES(string actorid, SqliteProvider sqliteProvider)
+        {
+            var props = Actor.FromProducer(() => new ESDataActor(sqliteProvider, actorid));
+            var pid = Actor.Spawn(props);
             var result = true;
             while (result)
             {
@@ -41,20 +64,79 @@ namespace P008_Persistence
                 }
             }
             sqliteProvider.DeleteEventsAsync(actorid, 10).Wait();
-            
+        }
 
+        private static void CallSN(string actorid, SqliteProvider sqliteProvider)
+        {
+            var props = Actor.FromProducer(() => new SNDataActor(sqliteProvider, actorid));
+            var pid = Actor.Spawn(props);
+            var result = true;
+            while (result)
+            {
+                Console.WriteLine("1、Tell  2、删除持久化  3、退出");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        var random = new Random();
+                        var no = random.Next(5, 15);
+                        Console.WriteLine(no);
+                        pid.Tell(new Data { Amount = no });
+                        break;
+                    case "2":
+                        //完成处理后清理持久化的操作          
+                        sqliteProvider.DeleteEventsAsync(actorid, 10).Wait();
+                        break;
+                    case "3":
+                        result = false;
+                        break;
+                }
+            }
+            sqliteProvider.DeleteEventsAsync(actorid, 10).Wait();
+        }
+
+        private static void CallES_SN(string actorid, SqliteProvider sqliteProvider)
+        {
+            var props = Actor.FromProducer(() => new ES_SNDataActor(sqliteProvider, sqliteProvider, actorid));
+            var pid = Actor.Spawn(props);
+            var result = true;
+            while (result)
+            {
+                Console.WriteLine("1、Tell  2、删除持久化  3、退出");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        var random = new Random();
+                        var no = random.Next(5, 15);
+                        Console.WriteLine(no);
+                        pid.Tell(new Data { Amount = no });
+                        break;
+                    case "2":
+                        //完成处理后清理持久化的操作          
+                        sqliteProvider.DeleteEventsAsync(actorid, 10).Wait();
+                        break;
+                    case "3":
+                        result = false;
+                        break;
+                }
+            }
+            sqliteProvider.DeleteEventsAsync(actorid, 10).Wait();
         }
     }
+
     public class Data
     {
-        public int Amount { get; set; }
+        public long Amount { get; set; }
     }
-    public class DataActor : IActor
+
+    #region 事件溯源
+    public class ESDataActor : IActor
     {
-        private int _value = 0;
+        private long _value = 0;
         private readonly Persistence _persistence;
 
-        public DataActor(IEventStore eventStore, string actorId)
+        public ESDataActor(IEventStore eventStore, string actorId)
         {
             _persistence = Persistence.WithEventSourcing(eventStore, actorId, ApplyEvent);
         }
@@ -76,12 +158,100 @@ namespace P008_Persistence
                     await _persistence.RecoverStateAsync();
                     break;
                 case Data msg:
-                    if (msg.Amount > 7)
-                    {
-                        await _persistence.PersistEventAsync(new Data { Amount = msg.Amount });
-                    }
+                    await _persistence.PersistEventAsync(new Data { Amount = msg.Amount });
                     break;
             }
         }
     }
+    #endregion
+
+    #region 快照
+    public class SNDataActor : IActor
+    {
+        private long _value = 0;
+        private readonly Persistence _persistence;
+
+        public SNDataActor(ISnapshotStore snapshotStore, string actorId)
+        {
+            _persistence = Persistence.WithSnapshotting(snapshotStore, actorId, ApplySnapshot);
+        }
+        private void ApplySnapshot(Proto.Persistence.Snapshot snapshot)
+        {
+            switch (snapshot.State)
+            {
+                case long value:
+                    _value = value;
+                    break;
+            }
+        }
+        public async Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started _:
+                    await _persistence.RecoverStateAsync();
+                    break;
+                case Data msg:
+                    _value = _value + msg.Amount;
+                    await _persistence.DeleteSnapshotsAsync(10);
+                    await _persistence.PersistSnapshotAsync(_value);
+                    break;
+            }
+        }
+    }
+    #endregion
+
+    #region 事件溯源and快照
+    public class ES_SNDataActor : IActor
+    {
+        private long _value = 0;
+        private readonly Persistence _persistence;
+
+        public ES_SNDataActor(IEventStore @event, ISnapshotStore snapshotStore, string actorId)
+        {
+            _persistence = Persistence.WithEventSourcingAndSnapshotting(@event, snapshotStore, actorId, ApplyEvent, ApplySnapshot, new IntervalStrategy(10), () => { return _value; });
+        }
+        private void ApplyEvent(Proto.Persistence.Event @event)
+        {
+            switch (@event.Data)
+            {
+                case Data msg:
+                    _value = _value + msg.Amount;
+                    Console.WriteLine($"累计：{_value}");
+                    break;
+            }
+        }
+        private void ApplySnapshot(Proto.Persistence.Snapshot snapshot)
+        {
+            switch (snapshot.State)
+            {
+                case long value:
+                    _value = value;
+                    break;
+            }
+        }
+        public async Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started _:
+                    await _persistence.RecoverStateAsync();
+                    break;
+                case Data msg:
+                    await _persistence.PersistEventAsync(new Data { Amount = msg.Amount });
+                    if (ShouldTakeSnapshot())
+                    {
+                        //await _persistence.DeleteSnapshotsAsync(10);
+                        await _persistence.PersistSnapshotAsync(_value);
+                    }
+                    break;
+            }
+        }
+
+        private bool ShouldTakeSnapshot()
+        {
+            return true;
+        }
+    }
+    #endregion
 }
--- ProtoActorSample/P009_Server/Program.cs
+++ ProtoActorSample/P009_Server/Program.cs
@@ -16,30 +16,34 @@ namespace P009_Server
             Console.Title = "服务端";
             Console.WriteLine("回车开始");
             Console.ReadLine();
+            //设置序列化类型
             var wire = new WireSerializer(new[] { typeof(HelloRequest), typeof(HelloResponse) });
             Serialization.RegisterSerializer(wire, true);
-            var props = Actor.FromFunc(ctx =>
-            {
-                switch (ctx.Message)
-                {
-                    case HelloRequest msg:
-                        Console.WriteLine(msg.Message);
-                        ctx.Respond(new HelloResponse
-                        {
-                            Message = "回应：我是服务端",
-                        });
-                        break;
-                    default:
-                        break;
-                }
-                return Actor.Done;
-            });
 
+            var props = Actor.FromProducer(() => new HelloQuestActor());         
             Remote.RegisterKnownKind("hello", props);
             Remote.Start("127.0.0.1", 12000);
             Console.WriteLine("服务端开始……");
             Console.ReadLine();
         }
     }
- 
+
+    class HelloQuestActor : IActor
+    {
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case HelloRequest msg:
+                    Console.WriteLine(msg.Message);
+                    context.Respond(new HelloResponse
+                    {
+                        Message = "回应：我是服务端",
+                    });
+                    break;
+            }
+            return Actor.Done;
+        }
+    }
+
 }
